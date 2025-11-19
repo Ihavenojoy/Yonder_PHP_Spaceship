7f3c80330a738c40ede552aae242714473ff802f
Ihavenojoy <tom.van.de.lest@gmail.com>
2025-11-19 10:19:37 +0100

    update gitignore
    Index stats changed

--- PHP/Index.php
+++ PHP/Index.php
@@ -16,25 +16,10 @@ require_once 'Dal/WeaponDAL.php';
 require_once 'Scripts/Battle.php';
 
 
-$TestArmory = new Armory("Test Armory",25,25);
-$TestWeapon = new Weapon("Handgun",1,4,12,3);
-$TestArmory->AddWeapon($TestWeapon);
 
-foreach ($TestArmory->Weapons as $weapon)
-{
-    echo("\nIn {$TestArmory->Name}: {$weapon}");
-}
-$TestConnection = new DB_Connector();
-$TestConnection->Test_Connection();
-$TestWeaponDAL = new WeaponDAL();
-$TestWeapon = $TestWeaponDAL->GetWeapon(1);
-if (!$TestWeapon == null)
-{
-    Echo("\n{$TestWeapon}");
-}
 $ChallengerFleet = new Fleet("ChallengerFleet");
 for ($i = 0; $i < 4; $i++) {
-    $ship = new Spaceship("Challenger", 20, 8);
+    $ship = new Spaceship("Challenger", 120, 8);
     for ($c = 0; $c < 8; $c++) {
         $ship->AddCanon(new Canon("ChallengerCanon", 1, 3, 0, 2));
     }
@@ -43,7 +28,7 @@ for ($i = 0; $i < 4; $i++) {
 
 $DestroyerFleet = new Fleet("DestroyerFleet");
 for ($i = 0; $i < 8; $i++) {
-    $ship = new Spaceship("Destroyer", 10, 4);
+    $ship = new Spaceship("Destroyer", 60, 4);
     for ($c = 0; $c < 2; $c++) {
         $ship->AddCanon(new Canon("DestroyerCanon", 1, 2, 0, 1));
     }
@@ -51,7 +36,7 @@ for ($i = 0; $i < 8; $i++) {
 }
 $FalconFleet = new Fleet("FalconFleet");
 for ($i = 0; $i < 5; $i++) {
-    $ship = new Spaceship("Falcon-$i", 15, 6);
+    $ship = new Spaceship("Falcon-$i", 75, 6);
     for ($c = 0; $c < 3; $c++) {
         $ship->AddCanon(new Canon("FalconCanon-$i-$c", 2, 4, 0, 3));
     }
@@ -60,7 +45,7 @@ for ($i = 0; $i < 5; $i++) {
 
 $ViperFleet = new Fleet("ViperFleet");
 for ($i = 0; $i < 6; $i++) {
-    $ship = new Spaceship("Viper-$i", 12, 5);
+    $ship = new Spaceship("Viper-$i", 55, 5);
     for ($c = 0; $c < 4; $c++) {
         $ship->AddCanon(new Canon("ViperCanon-$i-$c", 1, 3, 0, 2));
     }
